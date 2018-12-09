e84b1bb5868754f7eeb296cd8599ab1f8913cee1
Chris Hunt <chris.hunt@hudl.com>
2018-12-09 11:11:03 +0000

    Tidy up day 3

--- day3.js
+++ day3.js
@@ -1,20 +1,4 @@
-var fs = require('fs')
-var data = fs.readFileSync('./data/day3.txt', {encoding: 'utf-8'})
-data = data.split('\n')
-let claims = data.map(row => {
-  row = row.split('@')
-  dims = row[1].split(':')
-  pos = dims[0].split(',')
-  size = dims[1].split('x') 
-  return {
-    claim: row[0].trim(),
-    left: parseInt(pos[0], 10),
-    top: parseInt(pos[1], 10),
-    width: parseInt(size[0], 10),
-    height: parseInt(size[1], 10)
-  }
-})
-
+let claims = getAndParseClaims()
 let maxWidth = 0
 let maxHeight = 0
 
@@ -44,6 +28,9 @@ for (let x = 0; x < maxWidth; x ++) {
   }
 }
 
+console.log(`Part 1: Multiple claimed parts - ${multiClaim}`)
+
+// Find Unique claimeconsole
 claims.map(claim => {
   let uniqueClaim = true
   for (let x = claim.left; x < claim.left + claim.width; x ++) {
@@ -54,11 +41,29 @@ claims.map(claim => {
     }
   }
   if (uniqueClaim) {
-    console.log("Unique claim: ", claim.claim)
+    console.log("Part 2: Unique claim - ", claim.claim)
   }
 })
 
-
+function getAndParseClaims() {
+  var fs = require('fs')
+  var data = fs.readFileSync('./data/day3.txt', {encoding: 'utf-8'})
+  data = data.split('\n')
+  let claims = data.map(row => {
+    row = row.split('@')
+    dims = row[1].split(':')
+    pos = dims[0].split(',')
+    size = dims[1].split('x') 
+    return {
+      claim: row[0].trim(),
+      left: parseInt(pos[0], 10),
+      top: parseInt(pos[1], 10),
+      width: parseInt(size[0], 10),
+      height: parseInt(size[1], 10)
+    }
+  })
+  return claims
+}
 
 // Helpers
 function zeros(dimensions) {
